d8096fccabe7775c35f0bb432c762fde82ddd1c6
zozzz <vetesi.zoltan@gmail.com>
2024-02-26 22:37:26 +0100

    fix(layout): ExportAs nuDockingContent

--- packages/layout/src/docking/docking-content.component.ts
+++ packages/layout/src/docking/docking-content.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core"
 @Component({
     standalone: true,
     selector: "nu-docking-content",
+    exportAs: "nuDockingContent",
     styleUrl: "./docking-content.component.scss",
     template: `<ng-content></ng-content>`
 })
