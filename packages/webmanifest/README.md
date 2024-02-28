aa012b0db88f3a7d8ec31cb2d25ec12fee4e3bb9
zozzz <vetesi.zoltan@gmail.com>
2024-02-28 20:39:52 +0100

    chore(webmanifest): Add webmanifest generator

---
+++ packages/webmanifest/README.md
@@ -0,0 +1,40 @@
+# webmanifest
+
+## Usage
+
+Install
+```sh
+nx add @ngutil/webmanifest
+```
+
+Add to `project.json`
+```json
+{
+    // ...
+    "targets": {
+        // ...
+        "webmanifest": {
+            "executor": "@ngutil/webmanifest:generate",
+            "options": {
+                // ...
+            }
+        }
+    }
+}
+```
+
+For availbale options run:
+
+```sh
+nx @ngutil/webmanifest:generate --help
+```
+
+## Autmatically copy options from `package.json` if not present in `manifest` option
+
+| `manifest` | `package.json` |
+| ---------- | -------------- |
+| `version` | `version` |
+| `appName` | `name` |
+| `appDescription` | `description` |
+| `developerName` | `author.name` |
+| `developerURL` | `author.url` |
---
+++ packages/webmanifest/src/executors/generate/executor.spec.ts
@@ -0,0 +1,46 @@
+// import { GenerateExecutorSchema } from "./schema"
+// const options: GenerateExecutorSchema = {}
+import * as fs from "fs"
+import * as path from "path"
+
+import executor from "./executor"
+
+const SELF_DIR = __dirname
+const ASSETS_DIR = path.join(SELF_DIR, "test-assets")
+const OUTPUT_DIR = path.join(SELF_DIR, "..", "..", "..", "..", "..", "dist", ".webmanifest-test")
+
+describe("Generate webmanifest", () => {
+    it("can run", async () => {
+        const iconPath = path.join(ASSETS_DIR, "icon.png")
+        const inconContent = fs.readFileSync(iconPath)
+        const indexHtml = path.join(ASSETS_DIR, "index.html")
+        const indexContent = fs.readFileSync(indexHtml, { encoding: "utf-8" })
+        const indexHtmlOutput = path.join(OUTPUT_DIR, "index.html")
+
+        const output = await executor({
+            iconPath: iconPath,
+            indexHtml: indexHtml,
+            indexHtmlOutput: indexHtmlOutput,
+            indexHtmlReplaceTag: "XYZ",
+            outputPath: path.join(OUTPUT_DIR, "manifest"),
+            packageJson: path.join(ASSETS_DIR, "package.json"),
+            manifest: {
+                path: "assets/manifest",
+                appName: "TestApp",
+                appShortName: "Test applicaton"
+            },
+            clean: true
+        })
+
+        expect(output.success).toStrictEqual(true)
+
+        const indexContent2 = fs.readFileSync(indexHtml, { encoding: "utf-8" })
+        expect(indexContent).toStrictEqual(indexContent2)
+
+        const newIndexContent = fs.readFileSync(indexHtmlOutput)
+        expect(indexContent).not.toStrictEqual(newIndexContent)
+
+        const inconContent2 = fs.readFileSync(iconPath)
+        expect(inconContent).toStrictEqual(inconContent2)
+    })
+})
