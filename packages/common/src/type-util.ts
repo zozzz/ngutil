4b19c46d0a4f17bf3879209f5cff2e0bf5333e8f
zozzz <vetesi.zoltan@gmail.com>
2024-02-28 21:38:46 +0100

    feat(aria): Add NumberRange type helper

---
+++ packages/common/src/type-util.ts
@@ -0,0 +1,8 @@
+type Enumerate<N extends number, Acc extends number[] = []> = Acc["length"] extends N
+    ? Acc[number]
+    : Enumerate<N, [...Acc, Acc["length"]]>
+
+/**
+ * type XY = NumberRange<0, 10>
+ */
+export type NumberRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
