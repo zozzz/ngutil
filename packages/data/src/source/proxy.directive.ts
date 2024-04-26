48750591062e5f9fa5bcb2fde63ddf17f244294a
zozzz <vetesi.zoltan@gmail.com>
2024-04-26 22:57:23 +0200

    fix(data)!: rename DataSourceProxy.source -> DataSourceProxy.value

--- packages/data/src/source/proxy.directive.ts
+++ packages/data/src/source/proxy.directive.ts
@@ -1,6 +1,6 @@
 import { DataSource as CdkDataSource, CollectionViewer } from "@angular/cdk/collections"
-import { Directive, Input, OnDestroy, Optional } from "@angular/core"
-import { toSignal } from "@angular/core/rxjs-interop"
+import { Directive, Input, OnDestroy, Optional, input } from "@angular/core"
+import { takeUntilDestroyed, toSignal } from "@angular/core/rxjs-interop"
 
 import {
     combineLatest,
@@ -63,10 +63,10 @@ export type DataSourceInput<T extends Model> = any
 })
 export class DataSourceProxy<T extends Model = Model>
     extends CdkDataSource<T | undefined>
-    implements OnDestroy, ConnectProtocol
-{
+    implements OnDestroy, ConnectProtocol {
+
     @Input({ required: true, alias: "nuDataSource" })
-    set source(value: DataSourceInput<T>) {
+    set value(value: DataSourceInput<T>) {
         this.#valueSub?.unsubscribe()
         this.#valueSub = coerceDataSource<T>(value)
             .pipe(
@@ -76,17 +76,24 @@ export class DataSourceProxy<T extends Model = Model>
                     }
                 })
             )
-            .subscribe(this.source$)
+            .subscribe(this.#value)
     }
-    #valueSub?: Subscription
-    readonly source$ = new ReplaySubject<DataSource<T>>(1)
 
-    readonly items$ = this.source$.pipe(
+    get value(): DataSource<T> {
+        return this.#valueSig()!
+    }
+
+    #valueSub?: Subscription
+    readonly #value = new ReplaySubject<DataSource<T>>(1)
+    readonly value$ = this.#value.pipe(takeUntilDestroyed())
+    readonly #valueSig = toSignal(this.value$)
+
+    readonly items$ = this.value$.pipe(
         switchMap(value => value.items$),
         share()
     )
 
-    readonly busy$ = this.source$.pipe(
+    readonly busy$ = this.value$.pipe(
         switchMap(value => value.busy$),
         share()
     )
@@ -97,7 +104,6 @@ export class DataSourceProxy<T extends Model = Model>
         this.#filter.next(value)
     }
     readonly #filter = new ReplaySubject<Filter<T>>(1)
-    // TODO: maybe mergedFilter$ = this.value$.pipe(switchMap(value => value.filter.merged$))
 
     @Input()
     set sorter(value: Sorter<T>) {
@@ -127,25 +133,25 @@ export class DataSourceProxy<T extends Model = Model>
         }
 
         this.#subs.add(
-            combineLatest({ src: this.source$, filter: this.#filter }).subscribe(({ src, filter }) => {
+            combineLatest({ src: this.value$, filter: this.#filter }).subscribe(({ src, filter }) => {
                 src.filter.forced.set(filter)
             })
         )
 
         this.#subs.add(
-            combineLatest({ src: this.source$, sorter: this.#sorter }).subscribe(({ src, sorter }) => {
+            combineLatest({ src: this.value$, sorter: this.#sorter }).subscribe(({ src, sorter }) => {
                 src.sorter.forced.set(sorter)
             })
         )
 
         this.#subs.add(
-            combineLatest({ src: this.source$, grouper: this.#grouper }).subscribe(({ src, grouper }) => {
+            combineLatest({ src: this.value$, grouper: this.#grouper }).subscribe(({ src, grouper }) => {
                 src.grouper.forced.set(grouper)
             })
         )
 
         this.#subs.add(
-            combineLatest({ src: this.source$, slimer: this.#slimer }).subscribe(({ src, slimer }) => {
+            combineLatest({ src: this.value$, slimer: this.#slimer }).subscribe(({ src, slimer }) => {
                 src.slimer.forced.set(slimer)
             })
         )
@@ -159,7 +165,7 @@ export class DataSourceProxy<T extends Model = Model>
         this.#cvSubs.get(collectionViewer)?.next()
         this.#cvSubs.set(collectionViewer, until)
 
-        return this.source$.pipe(
+        return this.value$.pipe(
             switchMap(value => value.connect(collectionViewer)),
             takeUntil(until),
             finalize(() => this.#cvSubs.delete(collectionViewer))
@@ -180,7 +186,7 @@ export class DataSourceProxy<T extends Model = Model>
 
 function coerceDataSource<T extends Model>(value: DataSourceInput<T>): Observable<DataSource<T>> {
     if (value instanceof DataSourceProxy) {
-        return value.source$
+        return value.value$
     } else if (value instanceof DataSource) {
         return of(value)
     } else if (value instanceof DataProvider) {
