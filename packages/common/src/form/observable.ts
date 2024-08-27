beb0ca43f0c0c446b35facbf21e9e785dc275e8f
zozzz <vetesi.zoltan@gmail.com>
2024-08-27 12:04:03 +0200

    ...

--- packages/common/src/form/observable.ts
+++ packages/common/src/form/observable.ts
@@ -5,9 +5,16 @@ import { debounceTime, distinctUntilChanged, map, Observable, startWith } from "
 
 import { isEqual } from "lodash"
 
+import { isFalsy } from "../util"
+
 export interface ObservableOptions {
     distinct?: boolean
     destruct?: boolean
+}
+
+export interface ObservableValueOptions extends ObservableOptions {
+    debounce?: number
+    falsyToNull?: boolean
 }
 
 export type FormControlProperties =
@@ -22,10 +29,14 @@ export type FormControlProperties =
     | "enabled"
     | "errors"
 
-export const DEFAULT = { distinct: true, destruct: true }
+export const DEFAULT: ObservableOptions = { distinct: true, destruct: true }
+export const VALUE_DEFAULT: ObservableValueOptions = { ...DEFAULT, falsyToNull: true }
 
-export function fcObservableValue<T>(fc: FormControl<T>, opts: ObservableOptions & { debounce?: number } = DEFAULT) {
-    let result = fc.valueChanges.pipe(
+export function fcObservableValue<T, O extends ObservableValueOptions>(
+    fc: FormControl<T>,
+    opts: O = VALUE_DEFAULT as any
+): Observable<O extends { falsyToNull: true } ? T | null : T> {
+    let result: Observable<any> = fc.valueChanges.pipe(
         startWith(null),
         map(() => fc.value)
     )
@@ -34,7 +45,11 @@ export function fcObservableValue<T>(fc: FormControl<T>, opts: ObservableOptions
         result = result.pipe(debounceTime(opts.debounce))
     }
 
-    return handleCommonOptions(result, opts)
+    if (opts.falsyToNull) {
+        result = result.pipe(map(v => (isFalsy(v) ? null : v)))
+    }
+
+    return handleCommonOptions(result, opts) as any
 }
 
 export function fcObservableProperty<T, P extends FormControlProperties>(
