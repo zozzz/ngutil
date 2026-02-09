0e7f2bfbc9b7dc52b94a170464c582bb21fe60d5
Zoltan Vetesi <vetesi.zoltan@gmail.com>
2026-02-09 22:57:07 +0100

    fix(style): PositionWatcher watch correctly inside fixed or sticky element

--- packages/style/src/services/position-watcher.service.ts
+++ packages/style/src/services/position-watcher.service.ts
@@ -1,7 +1,7 @@
 
 import { inject, Injectable, NgZone, DOCUMENT } from "@angular/core"
 
-import { distinctUntilChanged, Observable, of, shareReplay, Subscriber } from "rxjs"
+import { combineLatest, distinctUntilChanged, map, Observable, of, shareReplay, Subscriber } from "rxjs"
 
 import { isEqual } from "es-toolkit"
 
@@ -9,29 +9,38 @@ import { coerceElement, ElementInput } from "@ngutil/common"
 
 import { Position } from "../util/rect"
 
+export type WatchPosition = "document" | "viewport"
+export type Watches = Map<HTMLElement | Window, Observable<Position>>
+
 @Injectable({ providedIn: "root" })
 export class PositionWatcher {
     readonly #zone = inject(NgZone)
     readonly #document = inject(DOCUMENT)
-    readonly #watches: Map<HTMLElement, Observable<Position>> = new Map()
+    readonly #watches: Record<WatchPosition, Watches> = {} as any
 
-    watch(element: ElementInput | Window): Observable<Position> {
+    watch(element: ElementInput | Window, position: WatchPosition = "viewport"): Observable<Position> {
         if (element instanceof Window) {
             return of({ x: 0, y: 0 })
         }
 
         element = coerceElement(element)
 
-        let watcher = this.#watches.get(element)
+        const watchers = this.#watches[position] ??= new Map()
+
+        let watcher = watchers.get(element)
         if (watcher == null) {
-            watcher = this.#createWatcher(element)
-            this.#watches.set(element, watcher)
+            if (position === "document") {
+                watcher = this.#createDocumentWatcher(element)
+            } else {
+                watcher = this.#createViewportWatcher(element)
+            }
+            watchers.set(element, watcher)
         }
 
         return watcher
     }
 
-    #createWatcher(element: HTMLElement): Observable<Position> {
+    #createDocumentWatcher(element: HTMLElement): Observable<Position> {
         return this.#zone.runOutsideAngular(() =>
             new Observable((dest: Subscriber<Position>) => {
                 let rafId: number | undefined = undefined
@@ -51,5 +60,37 @@ export class PositionWatcher {
                 }
             }).pipe(distinctUntilChanged(isEqual), shareReplay(1))
         )
+    }
+
+    #createViewportWatcher(element: HTMLElement): Observable<Position> {
+        const relative = this.#relativeElement(element)
+        if (relative == null) {
+            return this.#createDocumentWatcher(element)
+        }
+
+        return this.#zone.runOutsideAngular(() => {
+            const relativePosition$ = this.watch(relative)
+            const elementPosition$ = this.#createDocumentWatcher(element)
+
+            return combineLatest({ relative: relativePosition$, element: elementPosition$ }).pipe(
+                map(({ relative, element }) => ({
+                    x: element.x - relative.x,
+                    y: element.y - relative.y
+                })),
+                shareReplay(1)
+            )
+        })
+    }
+
+    #relativeElement(element: HTMLElement): HTMLElement | undefined {
+        let parent = element.parentElement
+        while (parent) {
+            const style = getComputedStyle(parent)
+            if (style.position === "sticky" || style.position === "fixed") {
+                return parent
+            }
+            parent = parent.parentElement
+        }
+        return undefined
     }
 }
